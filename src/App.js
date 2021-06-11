9cfe0025e4468628bf13085cae9f030277d54afb
aous mohammad <aous.mohammad97@gmail.com>
2021-06-11 22:30:47 +0300

    design layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/app.scss'
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <div className='container'>
+        <div className='row'>
+          <div className='col-12'>
+            <div className='product'>
+              <div className='price'></div>
+              <div className='triangle'></div>
+              <div className='sold'></div>
+              <div className='lists'></div>
+              <div className='counter'></div>
+              <div className='toolbar'></div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
